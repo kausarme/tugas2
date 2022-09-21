09b561eded6c228bbd226d0fa9d24675582dbf39
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-21 12:00:54 +0700

    Added initial_mywatchlist_data.json

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
 
-# Create your models here.
-# `watched`untuk mendeskripsikan film tersebut sudah ditonton atau belum
-# `title`ntuk mendeskripsikan judul film
-# `rating`untuk mendeskripsikan rating film dalam rentang 1 sampai dengan 5
-# `release_date`untuk mendeskripsikan kapan film dirilis
-# `review`untuk mendeskripsikan review untuk film tersebut
-
 class WatchlistItem(models.Model):
     watched = models.BooleanField()
     title = models.TextField()
-    rating = models.IntegerField(max_length=1)
+    rating = models.IntegerField()
     release_date = models.DateField()
     review = models.TextField()
