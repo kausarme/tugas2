0cd0921709f18612c0c1d6e13da583dbb1e32bf1
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-21 12:30:54 +0700

    Create Views.py for html

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -3,8 +3,14 @@ from mywatchlist.models import MyWatchList
 
 
 # Create your views here.
-def show_mywatchlist():
-    return None
+def show_mywatchlist(request):
+    watchlist_item = MyWatchList.objects.all()
+    context = {
+        'list_barang': watchlist_item,
+        'nama': 'Kausar Meutuwah',
+        'NPM': "2106630100",
+    }
+    return render(request, "mywatchlist.html", context)
 
 
 def show_mywatchlist_xml():
