b9535b3ea7c2ad6486d053987d82fc8dac902885
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-22 00:09:15 +0700

    Modify README.md

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -9,6 +9,7 @@ app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_mywatchlist, name='mywatchlist'),
+    path('html/', show_mywatchlist, name='mywatchlist'),
     path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
     path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
     path('xml/<int:id>', show_mywatchlist_xml_by_id, name='show_mywatchlist_xml_by_id'),
