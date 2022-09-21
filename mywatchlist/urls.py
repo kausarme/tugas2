40e223504215792e7d711a8220da7fbb08de464d
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-21 13:52:10 +0700

    Create Views.py for XML and Json

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from mywatchlist.views import show_mywatchlist
+from mywatchlist.views import show_mywatchlist_xml
+from mywatchlist.views import show_mywatchlist_xml_by_id
+from mywatchlist.views import show_mywatchlist_json
+from mywatchlist.views import show_mywatchlist_json_by_id
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_mywatchlist, name='mywatchlist'),
+    path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
+    path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
+    path('xml/<int:id>', show_mywatchlist_xml_by_id, name='show_mywatchlist_xml_by_id'),
+    path('json/<int:id>', show_mywatchlist_json_by_id, name='show_mywatchlist_json_by_id'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from mywatchlist.models import MyWatchList
 
 
@@ -13,17 +15,21 @@ def show_mywatchlist(request):
     return render(request, "mywatchlist.html", context)
 
 
-def show_mywatchlist_xml():
-    return None
+def show_mywatchlist_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 
-def show_mywatchlist_json():
-    return None
+def show_mywatchlist_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
-def show_mywatchlist_xml_by_id():
-    return None
+def show_mywatchlist_xml_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 
-def show_mywatchlist_json_by_id():
-    return None
+def show_mywatchlist_json_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
