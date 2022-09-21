7f6d828b92085772a8c177ac1064addbc490714c
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-21 12:12:10 +0700

    Create placeholder for views.py

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def show_mywatchlist():
+    return None
+
+
+def show_mywatchlist_xml():
+    return None
+
+
+def show_mywatchlist_json():
+    return None
+
+
+def show_mywatchlist_xml_by_id():
+    return None
+
+
+def show_mywatchlist_json_by_id():
     return None
