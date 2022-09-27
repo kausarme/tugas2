287114b211058cab90000605321eec1033781667
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-27 22:40:29 +0700

    Create Basic Views and Template

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from models import Task
+
 
 # Create your views here.
+def show_mywatchlist(request):
+    todolist_item = Task.objects.all()
+    context = {
+        'list_todo': todolist_item,
+        'nama': 'Kausar Meutuwah',
+        'NPM': "2106630100",
+    }
+    return render(request, "todolist.html", context)
