b3b449a8b28efafb2b8c4444685fa395cc0419d0
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-28 23:34:03 +0700

    added logout

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todolist.views import show_todolist, register, login_user
+from todolist.views import show_todolist, register, login_user,logout_user
 
 app_name = "todolist"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", show_todolist, name="show_todolist"),
     path("register/", register, name="register"),
     path('login/', login_user, name='login')
+    path('logout/', logout_user, name='logout')
 ]
