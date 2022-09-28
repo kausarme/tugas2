63e784613192aec9e2c862c734766992bc1e4cdd
Kausar Meutuwah <kausar.meutuwah@gmail.com>
2022-09-28 23:34:11 +0700

    added logout

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,6 +6,6 @@ app_name = "todolist"
 urlpatterns = [
     path("", show_todolist, name="show_todolist"),
     path("register/", register, name="register"),
-    path('login/', login_user, name='login')
-    path('logout/', logout_user, name='logout')
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
